c246e8eed0dd242f289efd30a75710998baa0c80
Seo, Heejeong <heejeong3394@gmail.com>
2022-10-20 16:25:00 +0900

    문자열을 정수로 바꾸기

---
+++ src/No10_ChangeToInt.java
@@ -0,0 +1,13 @@
+
+public class No10_ChangeToInt {
+	
+	public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+	public static void main(String[] args) {
+		No10_ChangeToInt c = new No10_ChangeToInt();
+		System.out.println(c.solution("468789"));
+	}
+
+}
