4a99398a58eec00118c6164c897f905f8e63eb15
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-03-29 14:37:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for(int i: numbers) {
+            sum += i;
+        }
+        
+        return 45-sum;
+    }
+}
