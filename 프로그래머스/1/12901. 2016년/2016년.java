bd0d472a0ecb75dab4dec78ce4b697a8119837bc
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-04-12 15:31:00 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
+      String answer = "";
         
-        // 요일 배열
-        String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+      // 각 월의 날짜
+      int[] lastDay = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+      String[] day ={"FRI","SAT","SUN","MON","TUE","WED","THU"};
         
-        Calendar cal = Calendar.getInstance();
-        cal.set(2016, a-1, b);
-        answer += day[cal.get(cal.DAY_OF_WEEK)-1];  // 1부터 시작이므로 -1
-        
-        return answer;
-    }
+      int totalDate = 0;
+      for(int i=0; i<a-1; i++){
+          totalDate += lastDay[i];
+      }
+
+      totalDate += b-1;
+      answer = day[totalDate % 7];
+
+      return answer;
+  }
 }
