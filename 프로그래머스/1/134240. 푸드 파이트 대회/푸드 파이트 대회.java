6742f5285d33b6faeca757fc8dd54c6932adfc59
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-04-25 21:25:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.18 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
+        StringBuilder player = new StringBuilder();
         
         for(int i=1; i<food.length; i++) {
-            answer += String.valueOf(i).repeat(food[i]/2);
+            player.append(String.valueOf(i).repeat(food[i]/2));
         }
         
-        StringBuilder onePlayer = new StringBuilder(answer);    // 한 선수의 음식
-        answer += "0";  // 물
-        answer += onePlayer.reverse().toString();
+        answer = player + "0";
+        answer += player.reverse();
         
         return answer;
     }
