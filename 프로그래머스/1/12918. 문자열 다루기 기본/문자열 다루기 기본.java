788ef0744eb68742b4a56f7f89e27ba2f5d6a412
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-05-09 21:02:53 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean solution(String s) {
         if(s.length() == 4 || s.length() == 6) {
-            for(int i=0; i<s.length(); i++) {
-                if(s.charAt(i) < '0' || s.charAt(i) > '9')
-                    return false;
+            try {
+                int i = Integer.parseInt(s);
+                return true;
             }
-            return true;
+            catch(Exception e) {
+                return false;
+            }
         }
         return false;
     }
