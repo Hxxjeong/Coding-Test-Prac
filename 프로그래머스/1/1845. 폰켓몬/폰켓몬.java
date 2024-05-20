ea4a1d37f9513e802f11772469b78486deb27db7
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-05-20 21:26:32 +0900

    [level 1] Title: 폰켓몬, Time: 6.33 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> phoneketmon = new HashMap<>();
+        Set<Integer> phoneketmon = new HashSet<>();
         Arrays.sort(nums);
         
         for(int i: nums) {
-            phoneketmon.put(i, phoneketmon.getOrDefault(i, 0) + 1);
+            phoneketmon.add(i);
         }
         
         return Math.min(phoneketmon.size(), nums.length/2);
