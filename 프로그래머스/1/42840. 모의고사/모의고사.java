fb8984a01ebfa8d3f79df34b74d86d841b21ff24
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-05-29 18:11:40 +0900

    [level 1] Title: 모의고사, Time: 1.39 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,23 +7,18 @@ class Solution {
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] score = new int[3];
+        int[] score = new int[3];   // 각 수포자들의 점수
         for(int i=0; i<answers.length; i++) {
-            if(answers[i] == p1[i%p1.length])
+            if(answers[i] == p1[i%5])
                 score[0]++;
-            if(answers[i] == p2[i%p2.length])
+            if(answers[i] == p2[i%8])
                 score[1]++;
-            if(answers[i] == p3[i%p3.length])
+            if(answers[i] == p3[i%10])
                 score[2]++;
         }
         
-        int max = score[0];
-        
-        // 최댓값 찾기
-        for(int i=0; i<score.length; i++) {
-            if(max < score[i])
-                max = score[i];
-        }
+        // 최댓값
+        int max = Arrays.stream(score).max().orElse(score[0]);
         
         // 최댓값과 같으면 추가
         for(int i=0; i<score.length; i++) {
