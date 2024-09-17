d413ed2c1b108a135d4e09eac3042e4d9d5a8549
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-09-17 19:21:09 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,30 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<N; i++) {
-            String input = br.readLine();
+        a: for(int i=0; i<T; i++) {
+            char[] input = br.readLine().toCharArray();
 
-            Stack<Character> stack = new Stack<>();
             boolean isVPS = true;
+            Stack<Character> stack = new Stack<>();
 
-            for(char c: input.toCharArray()) {
-                if(c == '(') stack.push(c);
+            for (char c: input) {
+                if (c == '(') stack.push(c);
                 else {
+                    // 닫힌 괄호일 때 스택이 비어 있으면 VPS가 아니므로 다음 문자열 검사
                     if(stack.isEmpty()) {
-                        isVPS = false;
-                        break;
+                        System.out.println("NO");
+                        continue a;
                     }
                     else stack.pop();
                 }
             }
 
-            if(!stack.isEmpty()) isVPS = false;
-
+            isVPS = stack.isEmpty();
             System.out.println(isVPS ? "YES" : "NO");
         }
 
