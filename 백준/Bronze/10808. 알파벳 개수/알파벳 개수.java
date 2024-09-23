4b194a9ce63f9bd497d2cb8b560248795f047882
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-09-23 13:30:23 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 120 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,17 +1,20 @@
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        int[] alphabet = new int[26];
-        for(int i=0; i<input.length(); i++) {
-            alphabet[input.charAt(i)-'a']++;
-        }
+        int[] alphabet = new int[26];   // 알파벳이 나온 횟수
 
-        for(int i: alphabet) System.out.print(i + " ");
+        for(char c: input.toCharArray())
+            alphabet[c-'a']++;  // 문자열을 돌면서 해당 인덱스에 횟수 추가
 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i: alphabet) bw.write(i + " ");
+
+        bw.close();
         br.close();
     }
 }
