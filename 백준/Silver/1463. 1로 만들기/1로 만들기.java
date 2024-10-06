54482e462300df337db489e7e7a2cc287030a47e
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-10-06 15:10:01 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18612 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    static int[] arr = new int[1_000_001];
 
-        int[] dp = new int[n+1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr[1] = 0;
+        arr[2] = 1;
+        arr[3] = 1;
 
-        // 1은 이미 1이므로 2부터 계산
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + 1;
-            // 2로 나누어 떨어질 떄
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
-            // 3으로 나누어 떨어질 때
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+        // 1부터 10까지: 0 1 1 2 3 2 3 3 2 3
+        // 2나 3으로 나뉘는 경우 이전의 인덱스를 참고해서 더 작은 값 / 그렇지 않으면 1을 뺀 인덱스 값 참고
+        for(int i=4; i<arr.length; i++) {
+            if(i % 3 == 0 && i % 2 == 0) {
+                arr[i] = Math.min(arr[i/3], arr[i/2]) + 1;
+            }
+            else if(i % 3 == 0) arr[i] = Math.min(arr[i/3] + 1, arr[i-1] + 1);
+            else if(i % 2 == 0) arr[i] = Math.min(arr[i/2] + 1, arr[i-1] + 1);
+            else arr[i] = arr[i-1] + 1;
         }
 
-        System.out.println(dp[n]);
-        
-        sc.close();
+        System.out.println(arr[N]);
+
+        br.close();
     }
 }
