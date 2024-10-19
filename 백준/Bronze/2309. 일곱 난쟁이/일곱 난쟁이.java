663e97ffe7e7db72f06f110673446aef8787521c
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-10-19 17:51:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,39 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] nanjaeng = new int[9];
+        int[] arr = new int[9];
         for(int i=0; i<9; i++) {
-            nanjaeng[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(nanjaeng);
+        int sum = Arrays.stream(arr).sum();
 
-        int sum = Arrays.stream(nanjaeng).sum();
-
-        int falseNanjaeng1 = 0;
-        int falseNanjaeng2 = 0;
-
-        // 전체 합에서 두 개의 합을 뺐을 때 100이 나오는 경우의 수
-        a:for(int i=0; i<9; i++) {
-            for(int j=1; j<9; j++) {
-                if(sum - (nanjaeng[i] + nanjaeng[j]) == 100) {
-                    falseNanjaeng1 = nanjaeng[i];
-                    falseNanjaeng2 = nanjaeng[j];
+        a: for(int i=0; i<8; i++) {
+            for(int j=i+1; j<9; j++) {
+                if(arr[i] + arr[j] == sum - 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
                     break a;
                 }
             }
         }
 
-        // 거짓 난쟁이 둘을 빼고 리스트에 추가
+        Arrays.sort(arr);
         for(int i=0; i<9; i++) {
-            if (nanjaeng[i] != falseNanjaeng1 && nanjaeng[i] != falseNanjaeng2)
-                System.out.println(nanjaeng[i]);
+            if(arr[i] != 0) System.out.println(arr[i]);
         }
 
         br.close();
