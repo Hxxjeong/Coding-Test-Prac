6a9aab95fb00e9a32aa4d1a6f64e369e42f7687d
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-10-26 18:14:51 +0900

    [Silver III] Title: N과 M (8), Time: 152 ms, Memory: 18708 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        String[] nums = br.readLine().split(" ");
+        arr = new int[N];
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(nums[i]);
+        Arrays.sort(arr);
+
+        int[] result = new int[M];
+
+        dfs(result, 0, 0);
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    public static void dfs(int[] result, int depth, int start) {
+        // 깊이가 M에 도달하면 출력
+        if (depth == M) {
+            for (int num: result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=start; i<arr.length; i++) {
+            result[depth] = arr[i];
+            dfs(result, depth+1, i);
+        }
+    }
+}
