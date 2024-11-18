0aeb519d2c1fcff4c359c15aa7510d44a0c3f333
희정 <112850550+Hxxjeong@users.noreply.github.com>
2024-11-18 20:44:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,43 +4,36 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        // 사용자 id: 인덱스
+        // 사용자 - 인덱스
         Map<String, Integer> id = new HashMap<>();
-        for(int i=0; i<id_list.length; i++) {
-            id.put(id_list[i], i);
+        for(int i=0; i<id_list.length; i++) id.put(id_list[i], i);
+        
+        Set<String> set = new HashSet<>();
+        for(String s: report) set.add(s);   // 중복 신고 제거
+        String[] arr = set.toArray(new String[0]);
+        
+        List<String> from = new ArrayList<>();  // 신고한 사람
+        List<String> to = new ArrayList<>();    // 신고 받은 사람
+        for(int i=0; i<arr.length; i++) {
+            String[] r = arr[i].split(" ");
+            from.add(r[0]);
+            to.add(r[1]);
         }
         
-        // 중복 신고 제거
-        HashSet<String> set = new HashSet<>();
-        for(String s: report)
-            set.add(s);
-        String[] list = set.toArray(new String[0]);
-        
-        List<String> reportor = new ArrayList<>();  // 신고한 사람
-        List<String> reported = new ArrayList<>();  // 신고 받은 사람
-        
-        // 신고자와 신고를 받은 자 리스트에 추가
-        for(int i=0; i<list.length; i++) {
-            String[] reportArr = list[i].split(" ");
-            reportor.add(reportArr[0]);
-            reported.add(reportArr[1]);
+        // 신고 받은 사람 - 횟수
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<to.size(); i++) {
+            map.put(to.get(i), map.getOrDefault(to.get(i), 0)+1);
         }
         
-        // 신고 받는 사람: 신고 받은 횟수
-        Map<String, Integer> countReport = new HashMap<>();
-        for(int i=0; i<reported.size(); i++) {
-            String reportedUser = reported.get(i);
-            countReport.put(reportedUser, countReport.getOrDefault(reportedUser, 0) + 1);
-        }
-        
-        // 신고 받은 횟수가 k번 이상이면 신고자에게 알림
-        List<Integer> indexList = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry: countReport.entrySet()) {
-            if(entry.getValue() >= k) {
-                String target = entry.getKey();
-                for (int i=0; i<reported.size(); i++) {
-                    if (reported.get(i).equals(target)) {
-                        String user = reportor.get(i);
+        // 신고 당한 횟수가 k번 이상이면 answer에 추가
+        for(Map.Entry<String, Integer> e: map.entrySet()) {
+            if(e.getValue() >= k) {
+                String reported = e.getKey();
+                for(int i=0; i<from.size(); i++) {
+                    // 해당 유저를 신고한 경우
+                    if(to.get(i).equals(reported)) {
+                        String user = from.get(i);  // from과 to의 인덱스 동일
                         answer[id.get(user)]++;
                     }
                 }
