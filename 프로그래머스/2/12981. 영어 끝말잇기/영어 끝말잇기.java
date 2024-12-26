23982c153f2b1170f40e0ec2994a7a07d6b83524
희정 <112850550+Hxxjeong@users.noreply.github.com>
2024-12-26 20:24:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.63 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        
+        List<String> list = new ArrayList<>();
+        list.add(words[0]);
+        
+        for(int i=1; i<words.length; i++) {
+            String past = words[i-1];
+            String now = words[i];
+            
+            if(!list.contains(words[i]) && past.charAt(past.length()-1) == now.charAt(0)) {
+                list.add(words[i]);
+            }
+            else {
+                answer[0] = (i%n) + 1; // n번째 사람
+                answer[1] = (int) Math.floor(i/(double)n)+1; // n번쨰 순서
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
