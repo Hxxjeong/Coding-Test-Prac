f5312038392613ec050eefaa269c4007e5a14ae0
희정 <112850550+Hxxjeong@users.noreply.github.com>
2025-01-05 14:40:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 636 ms, Memory: 61080 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        String[] numbers = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
-        // 각 인덱스까지의 합 배열에 저장
-        int[] sums = new int[numbers.length+1];
-        int sum = 0;
-        for(int i=1; i<sums.length; i++) {
-            sum += Integer.parseInt(numbers[i-1]);
-            sums[i] = sum;
+        input = br.readLine().split(" ");
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(input[i]);
+
+        int[] dp = new int[N+1];
+        dp[1] = arr[0];
+        for(int i=2; i<=N; i++) dp[i] = arr[i-1] + dp[i-1];
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M; i++) {
+            input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+
+            sb.append(dp[end]-dp[start-1]).append("\n");
         }
 
-        List<Integer> sumList = new ArrayList<>();
-        for(int i=0; i<Integer.parseInt(input[1]); i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+        System.out.println(sb);
 
-            sumList.add(sums[end] - sums[start-1]);
-        }
-
-        sumList.forEach(System.out::println);
+        br.close();
     }
 }
