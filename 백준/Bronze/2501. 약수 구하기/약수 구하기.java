ea810a99cbe0840345a4dfa03dcc20b03bf7a48c
희정 <112850550+Hxxjeong@users.noreply.github.com>
2025-10-22 07:59:17 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=N; i++) {
+            if(N % i == 0) list.add(i);
+        }
+
+        System.out.println(list.size() < K ? 0 : list.get(K-1));
+    }
+}
