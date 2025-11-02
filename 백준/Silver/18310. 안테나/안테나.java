dfd86812fb7eb07362bd77cc11c24528b752a7d8
희정 <112850550+Hxxjeong@users.noreply.github.com>
2025-11-02 19:30:18 +0900

    [Silver III] Title: 안테나, Time: 552 ms, Memory: 44232 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[(N-1) / 2]);     // 중앙값
+
+        br.close();
+    }
+}
